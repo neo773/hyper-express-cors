5f0392d93d4354e619f7fa75d1927a86f85a3574
Joshua <neo773@protonmail.com>
2022-08-13 18:30:02 +0530

    Fix TS for real this time

--- dist/useCORS.d.ts
+++ dist/useCORS.d.ts
@@ -1,11 +1,7 @@
-import HyperExpress from 'hyper-express'
-
 interface CORSOptions {
     origin: string;
     credentials: boolean;
     optionsRoute?: boolean;
 }
-
 declare const useCORS: (options: CORSOptions) => (request: HyperExpress.Request, response: HyperExpress.Response) => Promise<void>;
-
-export default useCORS
+export default useCORS;
